19578c299f59c2354ffe2b882b7483d000a0d758
Gibigabcsusz <gbr.szilagyi@gmail.com>
2023-04-07 15:48:20 +0200

    mesh generálás felderítése...

--- szim/tesztprogram/geom.cpp
+++ szim/tesztprogram/geom.cpp
@@ -1,10 +1,56 @@
+// Mesh for a 2D model of a transmission line discontinuity.
+// The transmission line pieces have a rectangular cross-section,
+// the first piece has width "fl" and length "fw", ("sl" and "sw" for the secon one).
+
 #include "mfem.hpp"
-#include <stdio.h>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
+using namespace mfem;
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Szia Babbab!\n");
+    // default values for command line parameters
+    const char *new_mesh_file = "discontinuity.mesh";
+    double fw = 0.04;
+    double sw = 0.02;
+    double fl = 0.1;
+    double sl = 0.1;
+    int nw = 8;
+    int nl = 30;
+    int order = 1;
+    
+    // process command line parameters
+    OptionsParser args(argc, argv);
+    args.AddOption(&new_mesh_file, "-m", "--mesh-out-file",
+                   "Output Mesh file to write.");
+    args.AddOption(&nw, "-nw", "--num-elements-width",
+                   "Number of elements along width.");
+    args.AddOption(&nl, "-nl", "--num-elements-length",
+                   "Number of elements along length.");
+    args.AddOption(&order, "-o", "--mesh-order",
+                   "Order (polynomial degree) of the mesh elements.");
+    args.Parse();
+    if (!args.Good())
+    {
+       args.PrintUsage(cout);
+       return 1;
+    }
+    args.PrintOptions(cout);
+    
+    // set finite element space to quads
+    Element::Type el_type = Element::QUADRILATERAL;
+    // generate initial mesh
+    Mesh mesh = Mesh::MakeCartesian2D(nw, nl, el_type, 1, max(fw,sw), fl+sl);
+    //Mesh mesh = Mesh(2,4,1);
+    mesh.Finalize();
+    
+
+    ofstream ofs(new_mesh_file);
+    ofs.precision(8);
+    mesh.Print(ofs);
+    ofs.close();
+
     return 0;
 }
