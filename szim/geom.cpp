dd5dacbc9d3b0e548ed93ecb900247fa4d1b8b53
Szilágyi Gábor <gbr.szilagyi@gmail.com>
2023-04-12 14:58:11 +0200

    mesh generálás hosszú debugolás, de még nem kész

--- szim/geom.cpp
+++ szim/geom.cpp
@@ -18,13 +18,13 @@ int main(int argc, char *argv[])
     // default values for command line parameters
     // width -> y coord, length -> x coord
     const char *new_mesh_file = "discontinuity.mesh";
-    double fw = 0.04;
-    double sw = 0.02;
+    double fw = 4;
+    double sw = 2;
     double offset = 0.0;
-    double fl = 0.1;
-    double sl = 0.1;
+    double fl = 10;
+    double sl = 10;
     int nw = 3;
-    int nl = 2;
+    int nl = 3;
     int order = 1;
     
     // process command line parameters
@@ -71,9 +71,9 @@ int main(int argc, char *argv[])
     double bot_width = (wider_width-narrower_width)/2.0+offset;
     double top_width = (wider_width-narrower_width)/2.0-offset;
     // number of widthwise elements that stick out on the bottom of the wider piece (at least one)
-    int num_w_el_bot = max(1, nw*(int)(round(bot_width/wider_width)));
+    int num_w_el_bot = max(1, (int)(round((double)nw*bot_width/wider_width)));
     // number of widthwise elements for the narrower piece (at least one)
-    int num_w_el_mid = max(1, min(nw-2,nw*(int)(round(narrower_width/wider_width))));
+    int num_w_el_mid = max(1, min(nw-2, (int)(round((double)nw*narrower_width/wider_width))));
     // number of widthwise elements that stick out on the top of the wider piece (at least one)
     int num_w_el_top = nw-num_w_el_bot-num_w_el_mid;
     double dy_bot = bot_width/(double)(num_w_el_bot);
@@ -84,23 +84,25 @@ int main(int argc, char *argv[])
     for(int i=0; i<num_w_el_bot+1; i++)
     {
         y += dy_bot;
-        ycoord.Append(y);
+        ycoord[i]=y;
     }
     // add node y coords that define the middle part
-    for(int i=0; i<num_w_el_mid; i++)
+    for(int i=num_w_el_bot+1; i<num_w_el_bot+1+num_w_el_mid; i++)
     {
         y += dy_mid;
-        ycoord.Append(y);
+        ycoord[i]=y;
     }
     // add node y coords that define the top part
-    for(int i=0; i<num_w_el_top; i++)
+    for(int i=num_w_el_bot+1+num_w_el_mid; i<num_w_el_bot+1+num_w_el_mid+num_w_el_top; i++)
     {
         y += dy_top;
-        ycoord.Append(y);
+        ycoord[i]=y;
     }
     // number of lengthwise elements of the first part
-    int num_l_el_first = max(1, nl*(int)(round(fl/(fl+sl))));
-    // number of lengthwise elements of the second part
+    //int num_l_el_first = max(1, nl*(int)(round(fl/(fl+sl))));
+    int num_l_el_first = max(1, (int)(round((double)nl*fl/(fl+sl))));
+    //printf("num_l_el_first = %d\n",num_l_el_first);
+	// number of lengthwise elements of the second part
     int num_l_el_second = nl-num_l_el_first;
     double dx_first = fl/(double)(num_l_el_first);
     double dx_second = sl/(double)(num_l_el_second);
@@ -109,37 +111,49 @@ int main(int argc, char *argv[])
     for(int i=0; i<num_l_el_first+1; i++)
     {
         x += dx_first;
-        xcoord.Append(x);
+        xcoord[i]=x;
     }
     // add node x coords that define the second part
-    for(int i=0; i<num_l_el_second; i++)
+    for(int i=num_l_el_first+1; i<num_l_el_first+1+num_l_el_second; i++)
     {
         x += dx_second;
-        xcoord.Append(x);
+        xcoord[i]=x;
     }
 
+	//printf("%f, %f, %f, %f, %f, %f\n", dy_bot, dy_mid, dy_top, dx_first, dx_second, 1.0/0.0);
 	// add middle part for both segments
 	Array2D<int> vi(nw+1,nl+1); // vertex indices
-	for(int row=0; row<num_w_el_mid; row++)
+	// add bottom edge vertices of the middle section
+	for(int col=0; col<nl+1; col++)
 	{
-		// add two left vertices of the first element of the row
-    	vi[0][num_w_el_bot+row] =
-			mesh->AddVertex(xcoord[0], ycoord[num_w_el_bot+row]);
-    	vi[0][num_w_el_bot+row+1] =
-			mesh->AddVertex(xcoord[0], ycoord[num_w_el_bot+row+1]);
+		vi[num_w_el_bot][col] =
+    		mesh->AddVertex(xcoord[col], ycoord[num_w_el_bot]);
+	}
+	for(int row=num_w_el_bot; row<num_w_el_bot+num_w_el_mid; row++)
+	{
+		// add top left vertex of the first element of the row
+    	vi[row+1][0] =
+			mesh->AddVertex(xcoord[0], ycoord[row+1]);
 		// add the rest of the vertices and the elements of the row
 		for(int col=0; col<nl; col++)
 		{
-    		vi[col+1][num_w_el_bot+row] =
-        		mesh->AddVertex(xcoord[col+1], ycoord[num_w_el_bot+row]);
-    		vi[col+1][num_w_el_bot+row+1] =
-        		mesh->AddVertex(xcoord[col+1], ycoord[num_w_el_bot+row+1]);
-			mesh->AddQuad
+    		vi[row+1][col+1] =
+        		mesh->AddVertex(xcoord[col+1], ycoord[row+1]);
+			mesh->AddQuad(vi[row][col], vi[row+1][col],
+				vi[row+1][col+1], vi[row][col+1]);
 		}
-		// add voundary edges on the two ends of the middle part (in the x/lentgh direction)
-		//TODO
+		// add boundary edges on the two ends of the middle section (in the x/lentgh direction)
+		// boundary has attribute=2 on the x=0 side (port 1), attr.=3 on the other end (port 2)
+		// and attr.=1 on the walls
+		//mesh->AddBdrSegment(vi[row][0], vi[row+1][0], 2);
+		//mesh->AddBdrSegment(vi[row][nl], vi[row+1][nl], 3);
 	}
-
+	vi.Print();
+	printf("xcoord:\n");
+	xcoord.Print(out, 10);
+	printf("ycoord:\n");
+	ycoord.Print(out, 10);
+	
 
 //    // add vertices for "first" transmission line piece
 //    for(int i=0; i<NY; i++)
@@ -171,7 +185,7 @@ int main(int argc, char *argv[])
 //        mesh->AddBdrSegment(vert_ind[i+1][NX-1], vert_ind[i][NX-1], 5);
 //    }
 //    
-//    mesh->FinalizeQuadMesh();
+    mesh->Finalize();
 
     ofstream ofs("discontinuity.mesh");
     ofs.precision(8);
